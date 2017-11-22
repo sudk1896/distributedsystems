33af343bf5f58b963daca1cc73bdd282f778d8da
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2017-11-22 17:14:54 +0100

    ic

--- src/main/java/distributedsystems/GitProtocol.java
+++ src/main/java/distributedsystems/GitProtocol.java
@@ -1,6 +1,7 @@
 package distributedsystems;
 
 import java.io.File;
+import java.util.List;
 
 /**
 Copyright 2017 Universita' degli Studi di Salerno
@@ -20,11 +21,41 @@ A P2P based Git protocol API.
 
 public interface GitProtocol {
 
+	/**
+	 * Creates new repository in a directory
+	 * @param _repo_name a String, the name of the repository.
+	 * @param _directory a File, the directory where create the repository.
+	 * @return true if it is correctly created, false otherwise.
+	 */
 	public boolean createRepository(String _repo_name, File _directory);
-	
-	public boolean addFilesToRepository(String _repo_name, File _directory);
-	
-	public boolean commit(String _repo_name, File _directory);
+	/**
+	 * Adds a list of File to the given local repository. 
+	 * @param _repo_name a String, the name of the repository.
+	 * @param files a list of Files to be added to the repository.
+	 * @return true if it is correctly added, false otherwise.
+	 */
+	public boolean addFilesToRepository(String _repo_name, List<File> files);
+	/**
+	 * Apply the changing to the files in  the local repository.
+	 * @param _repo_name a String, the name of the repository.
+	 * @param _message a String, the message for this commit.
+	 * @return true if it is correctly committed, false otherwise.
+	 */
+	public boolean commit(String _repo_name, String _message);
+	/**
+	 * Push all commits on the Network. If the status of the remote repository is changed, 
+	 * the push fails, asking for a pull.
+	 * @param _repo_name _repo_name a String, the name of the repository.
+	 * @return a String, operation message.
+	 */
+	public String push(String _repo_name);
+	/**
+	 * Pull the files from the Network. If there is a conflict, the system duplicates 
+	 * the files and the user should manually fix the conflict.
+	 * @param _repo_name _repo_name a String, the name of the repository.
+	 * @return a String, operation message.
+	 */
+	public String pull(String _repo_name);
 	
 	
 }
